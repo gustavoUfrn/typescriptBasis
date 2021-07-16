e6216adfc5bb340cd2a3493c5fbda0f673d37f44
gustavoUfrn <mxgugaa@gmail.com>
2021-07-16 11:22:33 -0300

    Criando -categoria

--- src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import CreateCourseService from "./CreateCourseService";
-
-export function CreateCourse(request: Request, response: Response) {
-    CreateCourseService.execute({ name: "nodeJS", educator: "Gustavo" });
-
-    return response.send();
-}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { CreateCourse } from "./routes";
+
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
-
-app.get("/", CreateCourse);
+app.use(express.json());
+app.use(categoriesRoutes);
 
 app.listen(3333);
