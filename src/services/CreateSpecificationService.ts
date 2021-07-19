d0d39accc701c6d7a62437ad03abf9f17e285cef
gustavoUfrn <mxgugaa@gmail.com>
2021-07-19 17:27:06 -0300

    Criando repositorio de Specifications

--- src/services/CreateSpecificationService.ts
+++ src/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationsRepository } from "../modules/cars/repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
 class CreateSpecificationService {
-    execute() {
-        console.log("Meu pau");
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+    execute({ description, name }: IRequest): void {
+        const specificationsAlradyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationsAlradyExists) {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
     }
 }
 
