e38692c31cb63e6bb4044ef5dbffde10dbbcba50
gustavoUfrn <mxgugaa@gmail.com>
2021-07-19 13:18:52 -0300

    Validando o cadastro de Categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,6 +9,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
+    const categoryAlreaydExists = categoriesRepository.findByName(name);
+
+    if (categoryAlreaydExists) {
+        return response.status(400).json({ Error: "Category already exists" });
+    }
+
     categoriesRepository.create({ name, description });
 
     return response.status(201).send();
