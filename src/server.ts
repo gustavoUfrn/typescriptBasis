cf3519a440b916ce9feee45fa22984bdbfe79f7d
gustavoUfrn <mxgugaa@gmail.com>
2021-07-20 10:14:19 -0300

    Criando useCases de categoria

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specifications.routes";
 
 const app = express();
+
 app.use(express.json());
+
 app.use("/categories", categoriesRoutes);
 app.use("/specifications", specificationRoutes);
 
